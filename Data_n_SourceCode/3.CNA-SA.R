a77177206621859f2d7d9dbeff93a5a7ebdc9192
hauldhut <hauldhut@gmail.com>
2020-10-15 00:09:12 +0700

    Add file

--- Data_n_SourceCode/3.CNA-SA.R
+++ Data_n_SourceCode/3.CNA-SA.R
@@ -4,14 +4,16 @@ set.seed(258)
 #change the order of column/patients of clinical data following the variable 'info'
 cli = c_clinical; rownames(cli) = c_clinical$PATIENT_ID
 grp=sub_grp; grp=as.data.frame(grp)
-cli_surv =cli_surv[rownames(grp),]
+cli_surv =cli[rownames(grp),]
+cli_surv = na_if(cli_surv,"") #transform "" (missing space) into NA value 
 survData<-cbind(cli_surv$OS_MONTHS, cli_surv$OS_STATUS=="DECEASED")
 
 #create survData
 rownames(survData)<- rownames(cli_surv)
 colnames(survData)<-c("time", "status")
-survData <- as.data.frame(survData)
+survData <- as.data.frame(survData) #coxph and survfit require input as data frame
 
+#fit a Cox proportional hazards regression model 
 coxFit1 <- coxph(
   Surv(time, status) ~ as.factor(sub_grp),
   data = survData,
@@ -19,6 +21,7 @@ coxFit1 <- coxph(
 )
 pcox=round(summary(coxFit1)$logtest[3],8);pcox #p-value = 1.498e-05  
 
+#compute an estimate of a survival curve
 mfit <- survfit(Surv(time, status == 1) ~ as.factor(sub_grp), data = survData)
 
 #visualization of survival rate between two groups
