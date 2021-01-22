628731f3810d48350e9991cefb939849f36ee8e8
hauldhut <hauldhut@gmail.com>
2021-01-22 03:52:50 +0000

    Update 2.Clinical-SA.R

--- Data_n_SourceCode/2.Clinical-SA.R
+++ Data_n_SourceCode/2.Clinical-SA.R
@@ -1,26 +1,24 @@
 #library
 if(!require(devtools)) install.packages("devtools")
-devtools::install_github("huynguyen250896/geneSA")
+devtools::install_github("huynguyen250896/geneSA", force=TRUE)
 library(geneSA)
-if(!require(rlist)) install.packages("https://cran.r-project.org/src/contrib/Archive/rlist/rlist_0.4.tar.gz", repos = NULL); library(rlist)
 
 # create event vector for RNASeq data
 #>median is up-regulated genes and <median is down regulated genes
-event_rna <- apply(exp_dri_norm,1, function(x) ifelse(x > median(x),"up","down"))
+event_rna <- apply(t(exp_dri_norm), 2, function(x) ifelse(x > median(x),"up","down")) %>% as.data.frame()
 # check how many altered samples we have
-table(event_rna)
-# event_rna
+table(as.matrix(event_rna))
 # down    up 
 # 29519 29505 
-event_rna <- as.data.frame(event_rna) #should be as data frame
 
 #create new column ‘status’ with survival event as binary
-clinical_exp$status <- clinical_exp$OS_STATUS
-clinical_exp$status <-ifelse(clinical_exp$status == "LIVING",0,1) #set event: die = 1, survive = 0
-#add time and event columns of clinical_exp to event_rna
-event_rna <- cbind(event_rna,clinical_exp[,3]) #time
-event_rna <- cbind(event_rna,clinical_exp[,6]) #event
-colnames(event_rna)[32:33] <- c("time", "event")
+clinical_exp <- clinical_exp %>%
+  mutate(status = ifelse(clinical_exp$OS_STATUS == "LIVING",0,1)) #set event: death = 1, alive = 0
 
-#check what driver genes are significantly correlated with outcome
-geneSA(genename = driver, event = event_rna)
+#Make sure samples that in event_rna are also included in rows of clinical_exp and in exactly the same order
+all(rownames(clinical_exp) == rownames(clinical_exp))
+#[1] TRUE
+
+#check which driver gene is significantly correlated with patient outcome
+geneSA(data = event_rna, time = clinical_exp$OS_MONTHS, status = clinical_exp$status)
+
